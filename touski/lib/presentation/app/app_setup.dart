983959ba1dc960685a7bd596224a2b14349e3718
Maria, Germain <maria.germain.1@ulaval.ca>
2025-12-24 14:48:50 -0500

    test workflow

--- touski/lib/presentation/app/app_setup.dart
+++ touski/lib/presentation/app/app_setup.dart
@@ -1,6 +1,3 @@
-import 'dart:ffi';
-
-import 'package:flutter/services.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get_it/get_it.dart';
 import 'package:http/http.dart' as http;
